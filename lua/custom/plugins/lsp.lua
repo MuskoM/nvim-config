62df4442de9207cbb3e85a2f280acc72f39ed056
MuskoM <git@musko.codes>
2024-12-08 20:25:30 +0100

    Day 8, format my code, set and forget

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -2,7 +2,29 @@ return {
   {
     'neovim/nvim-lspconfig',
     config = function()
-      require 'lspconfig'.lua_ls.setup{}
+      local lsp = require 'lspconfig'
+      lsp.lua_ls.setup {}
+      lsp.ruff.setup {}
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
+
+          -- Setup autoformatting
+          if client.supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end
+            })
+          end
+        end
+      })
+
+      -- Keymaps
+      vim.keymap.set('n', "<space>f", vim.lsp.buf.format, { desc = 'Format file' })
     end,
     dependencies = {
       {
