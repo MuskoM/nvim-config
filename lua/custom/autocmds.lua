44e306ae5a85e44893352f4a2e782fd4458ef212
MuskoM <mateusz@musko.codes>
2025-07-28 19:00:18 +0200

    Remove background colors

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -1,7 +1,18 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
-  group = vim.api.nvim_create_augroup('advent-highlight-yank', {clear = true}),
+  group = vim.api.nvim_create_augroup('advent-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank {timeout = 300}
+    vim.highlight.on_yank { timeout = 300 }
+  end
+})
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  desc = 'Removes background color',
+  group = vim.api.nvim_create_augroup('color-my-pencils', { clear = true }),
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
   end
 })
